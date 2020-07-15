bdd5da7006bc9686a8c911d7dd972630751bf1bf
Michael Waide <mvwaide@gmail.com>
2020-07-15 16:31:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,20 @@
 class Dog
+  attr_accessor :name
 
-	@@all = []
-	attr_accessor :name
+  @@all = []
 
-	def initialize(name)
-		@name = name
-		@@all << self
-	end
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
-	def self.clear_all
-		@@all.clear
+  def self.clear_all
+    @@all.clear
+  end
 
-	end
-
-	def self.all
-		@@all.each do |dog|
-			puts dog.name
-		end
-	end
-end 
+  def self.all
+    @@all.each do |d|
+      puts d.name
+    end
+  end
+end
